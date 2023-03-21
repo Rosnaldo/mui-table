63d7be08de0edd49864f7c050c2b0fadd25f1a8f
Andrey <andreytsuzuki@gmail.com>
2023-03-21 17:55:06 -0300

    validate redis_url env

--- backend/src/config.schema-validation.ts
+++ backend/src/config.schema-validation.ts
@@ -7,4 +7,5 @@ export const configSchemaValidation = Joi.object({
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
+  REDIS_URL: Joi.string().required(),
 });
